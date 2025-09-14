80ffc363c3d8133a9fd4124d6496da35d0fb99d9
yousef_khaled <snixx88@yahoo.com>
2025-09-14 19:42:30 +0300

    Add files via upload

---
+++ index/script.js
@@ -0,0 +1,7 @@
+function welcome(){
+    window.open("https://www.victoriassecret.com/us/vs/beauty-catalog/5000006636?brand=vs&collectionId=9cd3828f-bd75-41db-a577-3d57957f9b1a&limit=180&orderBy=REC&priceType=regular&productId=9d4366b2-1776-43ef-909a-f3ec643f1aa4&stackId=6eff0dde-6a28-4723-a241-81b66c69f218&genericId=11189389&choice=2457&product_position=2&stack_position=2&stack_count=6&dataSource=manual-collection")
+        }
+
+        function buy(){
+            window.open(" https://www.amazon.eg/-/en/Valentino-Donna-Born-Roma-100Ml/dp/B07YCLHHB4/ref=asc_df_B07YCLHHB4/?tag=egoshpadde-21&linkCode=df0&hvadid=545262152072&hvpos=&hvnetw=g&hvrand=9536392155163502362&hvpone=&hvptwo=&hvqmt=&hvdev=c&hvdvcmdl=&hvlocint=&hvlocphy=9073641&hvtargid=pla-825151774079&psc=1&mcid=7ef20ca8b7133a32b7f72d8f20a4e487")
+                }
